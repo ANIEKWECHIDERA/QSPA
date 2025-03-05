16a8da7df7bf10d2891248a42d74040570f28b23
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2025-03-05 16:49:22 +0100

    Refactor Firebase configuration and ESLint settings for improved clarity and consistency

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    es2021: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "no-undef": "error",
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
   },
   overrides: [
     {
